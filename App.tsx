21013994a20806fa46f9df425501f4cddf27657d
vikil143 <vikil.lakkavatri25@gmail.com>
2023-02-18 12:34:22 +0530

    all packages are installed

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import MainNavigator from '@myapp/routes';
+import { StyleSheet, View } from 'react-native';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MainNavigator />
     </View>
   );
 }
